abfb1ed82dd1362f00fa5bc49671925f95f79186
Sacha Meunier <sacha.meunier@student.hepl.be>
2026-07-20 16:15:01 +0200

    🎨 design(tailwind): improvements
    - Use class over data attribute for theme switcher
    - Add Geist font
    - Add missing properties
    - Style scrollbar
    - Store user's there preferences localy

--- resources/views/components/layouts/root.blade.php
+++ resources/views/components/layouts/root.blade.php
@@ -1,5 +1,10 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-theme="light">
+<html
+    lang="{{ str_replace('_', '-', app()->getLocale()) }}"
+    x-data="{ darkMode: localStorage.getItem('theme') === 'dark' }"
+    x-init="$watch('darkMode', val => { localStorage.setItem('theme', val ? 'dark' : 'light'); document.documentElement.classList.toggle('dark', val) })"
+    :class="{ 'dark': darkMode }"
+>
     <x-layouts.partials.head/>
 
     <x-layouts.partials.body>
