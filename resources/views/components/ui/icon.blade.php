3bfb80b09df7bdc2eebb11067d8f5f5d130e52e3
Sacha Meunier <sacha.meunier@student.hepl.be>
2026-07-20 16:17:30 +0200

    ✨ feat(component): add icon
    - Add a base icon component that can be used to wrap other icon's paths.
    - Add an icon as a reference

---
+++ resources/views/components/ui/icon.blade.php
@@ -0,0 +1,28 @@
+@props([
+    'size' => 'default',
+    'strokeWidth' => '2',
+])
+
+@php
+    // Size mapping
+    $sizeClasses = match($size) {
+        'xs' => 'size-3',
+        'sm' => 'size-4',
+        'default','lg' => 'size-5',
+    };
+@endphp
+
+<svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="{{ $strokeWidth }}"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="{{ $sizeClasses }} {{ $attributes->get('class') }}"
+    aria-hidden="true"
+    {{ $attributes->except(['class']) }}
+>
+    {{ $slot }}
+</svg>
