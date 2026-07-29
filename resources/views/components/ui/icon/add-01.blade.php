32e48b19c49ca666e61ec1ed5242dcf7d55184bb
Sacha Meunier <sacha.meunier@student.hepl.be>
2026-07-29 11:40:33 +0200

    ✨ feat(component): add icons

---
+++ resources/views/components/ui/icon/add-01.blade.php
@@ -0,0 +1,8 @@
+@blaze()
+
+{{-- Credit: Hugeicons (https://hugeicons.com) --}}
+
+<x-ui.icon {{ $attributes }}>
+    <path d="M12 4V20" />
+    <path d="M4 12H20" />
+</x-ui.icon>
---
+++ resources/views/components/ui/icon/delete-02.blade.php
@@ -0,0 +1,11 @@
+@blaze()
+
+{{-- Credit: Hugeicons (https://hugeicons.com) --}}
+
+<x-ui.icon {{ $attributes }}>
+    <path d="M4 7H20" />
+    <path d="M6 7L7 19C7 20.1046 7.89543 21 9 21H15C16.1046 21 17 20.1046 17 19L18 7" />
+    <path d="M9 7V4C9 3.44772 9.44772 3 10 3H14C14.5523 3 15 3.44772 15 4V7" />
+    <path d="M10 11V17" />
+    <path d="M14 11V17" />
+</x-ui.icon>
