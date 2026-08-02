737e1f0862cedf90916a3d8fa78d4d479d0331b2
Sacha Meunier <sacha.meunier@student.hepl.be>
2026-08-02 18:45:14 +0200

    🎨 design(sidebar-settings): add teams nesting & icons

--- resources/views/components/domain/app/sidebar/settings.blade.php
+++ resources/views/components/domain/app/sidebar/settings.blade.php
@@ -21,34 +21,37 @@
 
     <nav class="flex min-h-0 flex-1 flex-col items-start overflow-y-auto px-1">
         <x-domain.app.sidebar.group label="Personal">
-            <x-domain.app.sidebar.item :href="route('settings.account.profile')">Profile</x-domain.app.sidebar.item>
-            <x-domain.app.sidebar.item :href="route('settings.account.security')">Account & Security</x-domain.app.sidebar.item>
-            {{--<x-domain.app.sidebar.item :href="route('settings.account.preferences')">Preferences</x-domain.app.sidebar.item>--}}
-            {{--<x-domain.app.sidebar.item :href="route('settings.account.display')">Display</x-domain.app.sidebar.item>--}}
-            {{--<x-domain.app.sidebar.item :href="route('settings.account.tracking')">Tracking</x-domain.app.sidebar.item>--}}
-            {{--<x-domain.app.sidebar.item :href="route('settings.account.notifications')">Notifications</x-domain.app.sidebar.item>--}}
+            <x-domain.app.sidebar.item icon="user" :href="route('settings.account.profile')">Profile</x-domain.app.sidebar.item>
+            <x-domain.app.sidebar.item icon="square-lock-02" :href="route('settings.account.security')">Account & Security</x-domain.app.sidebar.item>
+            {{--<x-domain.app.sidebar.item icon="settings-02" :href="route('settings.account.preferences')">Preferences</x-domain.app.sidebar.item>--}}
+            {{--<x-domain.app.sidebar.item icon="computer" :href="route('settings.account.display')">Display</x-domain.app.sidebar.item>--}}
+            {{--<x-domain.app.sidebar.item icon="activity-01" :href="route('settings.account.tracking')">Tracking</x-domain.app.sidebar.item>--}}
+            {{--<x-domain.app.sidebar.item icon="notification-01" :href="route('settings.account.notifications')">Notifications</x-domain.app.sidebar.item>--}}
         </x-domain.app.sidebar.group>
 
         <x-domain.app.sidebar.group label="Workspace">
-            <x-domain.app.sidebar.item :href="route('settings.workspace.general')">General</x-domain.app.sidebar.item>
-            <x-domain.app.sidebar.item href="#" disabled>Members</x-domain.app.sidebar.item>
+            <x-domain.app.sidebar.item icon="settings-01" :href="route('settings.workspace.general')">General</x-domain.app.sidebar.item>
+            <x-domain.app.sidebar.item icon="user-group" href="#" disabled>Members</x-domain.app.sidebar.item>
             {{--<x-domain.app.sidebar.item href="#" disabled>Roles</x-domain.app.sidebar.item>--}}
             {{--<x-domain.app.sidebar.item href="#" disabled>Invitations</x-domain.app.sidebar.item>--}}
             {{--<x-domain.app.sidebar.item href="#" disabled>Billing</x-domain.app.sidebar.item>--}}
             {{--<x-domain.app.sidebar.item href="#" disabled>Activity & Confidentiality</x-domain.app.sidebar.item>--}}
-            <x-domain.app.sidebar.item href="#" disabled>Listing</x-domain.app.sidebar.item>
+            <x-domain.app.sidebar.item icon="store-01" href="#" disabled>Listing</x-domain.app.sidebar.item>
         </x-domain.app.sidebar.group>
 
-        @foreach ($teams as $team)
-            <x-domain.app.sidebar.group :label="$team->name">
-                <x-domain.app.sidebar.item :href="route('settings.teams.general', $team)">General</x-domain.app.sidebar.item>
-                <x-domain.app.sidebar.item href="#" disabled>Members</x-domain.app.sidebar.item>
-                {{--<x-domain.app.sidebar.item href="#" disabled>Roles</x-domain.app.sidebar.item>--}}
-                {{--<x-domain.app.sidebar.item href="#" disabled>Invitations</x-domain.app.sidebar.item>--}}
-                {{--<x-domain.app.sidebar.item href="#" disabled>Billing</x-domain.app.sidebar.item>--}}
-                {{--<x-domain.app.sidebar.item href="#" disabled>Activity & Confidentiality</x-domain.app.sidebar.item>--}}
-                <x-domain.app.sidebar.item :href="route('settings.teams.listing.index', $team)">Listing</x-domain.app.sidebar.item>
+        @if ($teams->isNotEmpty())
+            <x-domain.app.sidebar.group label="Teams">
+                @foreach ($teams as $team)
+                    <x-domain.app.sidebar.team :team="$team">
+                        <x-domain.app.sidebar.tree-branch>
+                            <x-domain.app.sidebar.item icon="settings-01" :href="route('settings.teams.general', $team)">General</x-domain.app.sidebar.item>
+                        </x-domain.app.sidebar.tree-branch>
+                        <x-domain.app.sidebar.tree-branch last>
+                            <x-domain.app.sidebar.item icon="store-01" :href="route('settings.teams.listing.index', $team)">Listing</x-domain.app.sidebar.item>
+                        </x-domain.app.sidebar.tree-branch>
+                    </x-domain.app.sidebar.team>
+                @endforeach
             </x-domain.app.sidebar.group>
-        @endforeach
+        @endif
     </nav>
 </aside>
