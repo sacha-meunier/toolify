afcdfa0365891ab4cd45719108731259138f5f66
Sacha Meunier <sacha.meunier@student.hepl.be>
2026-08-02 11:09:37 +0200

    👨🔧 patch(routes): add full view page path

--- routes/app/app.php
+++ routes/app/app.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::livewire('/search', 'pages::app.search')->name('search');
-    Route::livewire('/discovery', 'pages::app.discovery')->name('discovery');
+    Route::livewire('/search', 'pages::app.search.index')->name('search');
+    Route::livewire('/discovery', 'pages::app.discovery.index')->name('discovery');
     Route::livewire('/tools/{tool:slug}', 'pages::app.tools.show')->name('tools.show');
 
     Route::livewire('/stack', 'pages::app.stacks.personal')->name('stacks.personal');
