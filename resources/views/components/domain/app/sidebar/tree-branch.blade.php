b05fc835aa161eba282df9181dd42b5de98492a5
Sacha Meunier <sacha.meunier@student.hepl.be>
2026-09-01 16:19:38 +0200

    🎨 design(): update sidebar tree branch color

--- resources/views/components/domain/app/sidebar/tree-branch.blade.php
+++ resources/views/components/domain/app/sidebar/tree-branch.blade.php
@@ -1,10 +1,10 @@
 @props(['last' => false])
 
 <div class="relative flex w-full flex-col items-start pl-6">
-    <span class="absolute left-[18px] top-0 h-[18px] w-3 rounded-bl-xl border-b border-l border-sidebar-border"></span>
+    <span class="absolute left-[18px] top-0 h-[18px] w-3 rounded-bl-xl border-b border-l border-sidebar-tree-line"></span>
 
     @unless ($last)
-        <span class="absolute left-[18px] top-[5px] bottom-0 w-px bg-sidebar-border"></span>
+        <span class="absolute left-[18px] top-[5px] bottom-0 w-px bg-sidebar-tree-line"></span>
     @endunless
 
     {{ $slot }}
